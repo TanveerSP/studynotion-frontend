7a0dea1d5cdda34d9dafeeafbfc197e4b060610f
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-06-16 19:25:48 +0530

    change in index js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
 	cors({
 		origin: ["http://localhost:3000",
-			// "https://study-notion-lake.vercel.app",
+			"https://studynotion-backend-c9s6.onrender.com",
 			// "https://studynotion.tech"
 		],
 		credentials: true,
