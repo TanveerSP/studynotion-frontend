459a7602e5f94e02df4b2a1fd107f8a78f0c562d
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-06-16 19:30:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
 	cors({
 		origin: ["http://localhost:3000",
-			"https://studynotion-backend-c9s6.onrender.com",
+			//"https://studynotion-backend-c9s6.onrender.com",
 			// "https://studynotion.tech"
 		],
 		credentials: true,
